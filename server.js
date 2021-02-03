7f9b927e8ae6315b4ed2f773b99ef37242817b5c
NoahBlack012 <eastbo2004@gmail.com>
2021-02-02 20:15:27 -0500

    Added multiple routes for server

--- server.js
+++ server.js
@@ -6,7 +6,11 @@ const app = express()
 
 app.use('/', serveStatic(path.join(__dirname, '/dist')))
 
-const port = process.env.PORT || 8080
+app.get(/.*/, (req, res) => {
+	res.sendFile(path.join(__dirname, '/dist/index.html'))
+})
+
+const port = process.env.PORT || 8000
 app.listen(port)
 
 console.log(`Listening on port: ${port}`);
